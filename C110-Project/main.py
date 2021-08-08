559421fe5dd44bf35f7be7f8d6d88bff92bf98ad
sourabhvishnav <62550282+sourabhvishnav@users.noreply.github.com>
2021-08-08 09:23:01 +0530

    Add files via upload

--- C110-Project/main.py
+++ C110-Project/main.py
@@ -20,21 +20,40 @@ def show_fig(meanData):
     df = meanData
     sampling_mean = statistics.mean(meanData)
     sampling_std_deviation = statistics.stdev(meanData)
+    first_std_start, first_std_end = sampling_mean - sampling_std_deviation, sampling_mean + sampling_std_deviation
+    second_std_start, second_std_end = sampling_mean - (2*sampling_std_deviation), sampling_mean + (2*sampling_std_deviation)
+    third_std_start, third_std_end = sampling_mean - (3*sampling_std_deviation), sampling_mean + (3*sampling_std_deviation)
     zScore = (sampling_mean - population_mean) / population_std_deviation
     print("Z-score = ", zScore)
     print("Standard deviation of sampling distribution:- ", sampling_std_deviation)
     print("Mean of samling distribution is ", sampling_mean)
     fig = ff.create_distplot(
         [meanData], ["mean_of_1000_samples"], show_hist=False)
+    fig.add_trace(go.Scatter(x=[first_std_start, first_std_start], y=[
+              0, 0.18], mode="lines", name="first_std_deviation_start"))
+    fig.add_trace(go.Scatter(x=[first_std_end, first_std_end], y=[
+              0, 0.18], mode="lines", name="first_std_deviation_end"))
+
+    fig.add_trace(go.Scatter(x=[second_std_start, second_std_start], y=[
+              0, 0.18], mode="lines", name="second_std_deviation_start"))
+    fig.add_trace(go.Scatter(x=[second_std_end, second_std_end], y=[
+              0, 0.18], mode="lines", name="second_std_deviation_end"))
+
+    fig.add_trace(go.Scatter(x=[third_std_start, third_std_start], y=[
+              0, 0.18], mode="lines", name="third_std_deviation_start"))
+    fig.add_trace(go.Scatter(x=[third_std_end, third_std_end], y=[
+              0, 0.18], mode="lines", name="third_std_deviation_end"))    
     fig.add_trace(go.Scatter(x=[sampling_mean, sampling_mean], y=[
                   0, 1], mode="lines", name="Mean"))
     fig.show()
 
 
-def random_set_of_mean():
+
+
+def random_set_of_mean(counter):
     dataset = []
-    for z in range(0, 100):
-        random_index = random.randint(0, len(data)-1)
+    for z in range(0, counter):
+        random_index = random.randint(0, len(data))
         value = data[random_index]
         dataset.append(value)
     mean = statistics.mean(dataset)
@@ -43,8 +62,8 @@ def random_set_of_mean():
 
 def setup():
     mean_list = []
-    for i in range(0, 1000):
-        individualSampleMean = random_set_of_mean()
+    for i in range(0, 100):
+        individualSampleMean = random_set_of_mean(30)
         mean_list.append(individualSampleMean)
     show_fig(mean_list)
 
